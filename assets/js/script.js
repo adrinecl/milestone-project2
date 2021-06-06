b2b46a4b27fd8be2133c50e079c1f7daa1220667
adrinecl <adrine.lindstrom@icloud.com>
2021-06-06 10:34:54 +0000

    Add a worm moving around the game area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
-
 document.addEventListener("DOMContentLoaded", function () {
+    // This function is called each time the level, score or game over change.
+    const callback = (level, score, gameOver) => {
+        console.log(level, score, gameOver);
+    };
     const gameArea = document.getElementById('game-area')
-    const wormGame = new WormGame(gameArea, 20, 20);
+    const wormGame = new WormGame(gameArea, 20, 20, callback);
+
     document.onkeydown = (event) => {
         if (!event.repeat) {
             wormGame.keyDown(event.keyCode);
         }
     };
+
+    // Connect the "New Game" button on the pagew to the wormGame.
+    const newGame = document.getElementById('new-game');
+    newGame.addEventListener('click', () => {
+        wormGame.startNewGame();
+    });
 });
 
 class WormGame {
@@ -31,5 +41,197 @@ class WormGame {
                 this.area.appendChild(cell);
             }
         }
+    }
+
+    invokeCallback() {
+        this.callback(this.level, this.score, this.gameOver);
+    }
+
+    startNewGame() {
+        this.gameOver = false;
+        this.score = 0;
+        this.level = 1;
+        this.levelLengthGoal = 5;
+        this.speed = 200;
+        this.startNewLevel();
+        this.invokeCallback();
+    }
+
+    startNewLevel() {
+        this.createNewWorm();
+        this.resetArea();
+        this.resetGrid();
+        this.placeWorm();
+        this.resetGameTimer();
+        this.startGameTimer();
+    }
+
+    createNewWorm() {
+        const headRow = this.randomRow(3);
+        const headCol = this.randomRow(3);
+        const goRight = headCol < this.cols / 2;
+        const wormDir = goRight ? 1 : 3; // up, right, down, left
+        const tailRow = headRow;
+        const tailCol = goRight ? headCol - 1 : headCol + 1;
+        const head = {
+            row: headRow,
+            col: headCol,
+            color: this.randomColor()
+        };
+        const tail = {
+            row: tailRow,
+            col: tailCol,
+            color: this.randomColor()
+        };
+        this.turn = 0;
+        this.worm = {
+            parts: [head, tail],
+            direction: wormDir
+        };
+    }
+
+    randomRow(inset) {
+        return Math.floor(Math.random() * (this.rows - inset * 2)) + inset;
+    }
+
+    randomCol(inset) {
+        return Math.floor(Math.random() * (this.cols - inset * 2)) + inset;
+    }
+
+    randomColor() {
+        return Math.floor(Math.random() * 5);
+    }
+
+    resetArea() {
+        this.area.classList.remove('game-over');
+    }
+
+    resetGrid() {
+        for (let cell of this.area.children) {
+            delete cell.dataset.color;
+        }
+    }
+
+    placeWorm() {
+        for (let part of this.worm.parts) {
+            const cell = this.getCell(part.row, part.col);
+            cell.dataset.color = part.color;
+        }
+    }
+
+    endGame() {
+        this.gameOver = true;
+        this.area.classList.add('game-over');
+        this.resetGameTimer();
+    }
+
+    resetGameTimer() {
+        clearInterval(this.timer);
+        delete this.timer;
+    }
+
+    startGameTimer() {
+        this.timer = setInterval(() => this.move(), this.speed);
+    }
+
+    keyDown(keyCode) {
+        if (keyCode == 37 || keyCode == 39) {
+            this.turn = keyCode == 37 ? -1 : +1;
+        }
+    }
+
+    move() {
+        this.moveHead();
+        this.moveBody();
+        const tail = this.moveTail();
+
+        if (this.isGameOver()) {
+            this.endGame();
+            this.invokeCallback();
+            return;
+        }
+    }
+
+    moveHead() {
+        const head = this.worm.parts[0];
+        let row = head.row;
+        let col = head.col;
+        let dir = (this.worm.direction + this.turn + 4) % 4;
+
+        switch (dir) {
+            case 0:
+                row -= 1;
+                break;
+            case 1:
+                col += 1;
+                break;
+            case 2:
+                row += 1;
+                break;
+            case 3:
+                col -= 1;
+                break;
+        }
+
+        this.turn = 0;
+        this.worm.direction = dir;
+        this.worm.parts.unshift({
+            row: row,
+            col: col,
+            color: head.color
+        });
+
+        if (this.inGameArea(row, col)) {
+            const cell = this.getCell(row, col);
+            cell.dataset.color = head.color;
+        }
+    }
+
+    moveBody() {
+        for (let i = 1; i < this.worm.parts.length - 1; i++) {
+            const part = this.worm.parts[i];
+            const next = this.worm.parts[i + 1];
+            const cell = this.getCell(part.row, part.col);
+            part.color = next.color;
+            cell.dataset.color = part.color;
+        }
+    }
+
+    moveTail() {
+        const tail = this.worm.parts.pop();
+        const cell = this.getCell(tail.row, tail.col);
+        delete cell.dataset.color;
+        return tail;
+    }
+
+    isGameOver() {
+        const head = this.worm.parts[0];
+        if (!this.inGameArea(head.row, head.col)) {
+            console.log("Game over: hit a wall");
+            return true;
+        }
+        if (this.inWormBody(head.row, head.col)) {
+            console.log("Game over: hit the worm");
+            return true;
+        }
+        return false;
+    }
+
+    inGameArea(row, col) {
+        return row >= 0 && col >= 0 && row < this.rows && col < this.cols;
+    }
+
+    inWormBody(row, col) {
+        for (let i = 1; i < this.worm.parts.length; i++) {
+            const part = this.worm.parts[i];
+            if (row == part.row && col == part.col) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    getCell(row, col) {
+        return this.grid[row + ',' + col];
     }
 }
